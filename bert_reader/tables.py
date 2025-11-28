fc397351dcd414bbd96f9110d7c01f04fb1e5c2b
kovvik <kovvik@gmail.com>
2025-11-28 08:36:36 +0100

    IA-32 Architecture Non-Maskable Interrupt parsing implemented, #6

--- bert_reader/tables.py
+++ bert_reader/tables.py
@@ -435,6 +435,27 @@ class IA32ArchitectureCorrectedMachineCheck(HardwareErrorSourceEntry):
         self.data["length"] = 48 + self.data["number_of_hardware_banks"] * 28
 
 
+class IA32ArchitectureNMI(HardwareErrorSourceEntry):
+    """
+    IA-32 Architecture Non-Maskable Interrupt
+
+    18.3.2.3 in ACPI Specification.
+    """
+
+    def __init__(self, data):
+        self.name = "IA-32 Architecture Non-Maskable Interrupt"
+        self.data = {
+            "type": self.binary_to_int(data[0:2] + b"\00\00", 0, length=4),
+            "source_id": self.binary_to_int(
+                data[2:4] + b"\00\00", 0, length=4
+            ),
+            "number_of_records_to_preallocate": self.binary_to_int(data, 8),
+            "max_sections_per_record": self.binary_to_int(data, 12),
+            "max_raw_data_length": self.binary_to_int(data, 16),
+            "length": 20,
+        }
+
+
 class AERRootPort(HardwareErrorSourceEntry):
     """
     PCI Express Root Port AER Structure
@@ -600,7 +621,7 @@ class GenericHardwareErrorSourceV2(HardwareErrorSourceEntry):
 HEST_TYPES = {
     0: IA32ArchitectureMachineCheckException,
     1: IA32ArchitectureCorrectedMachineCheck,
-    2: "IA-32 Architecture NMI",
+    2: IA32ArchitectureNMI,
     6: AERRootPort,
     7: AEREndpoint,
     8: AERBridge,
